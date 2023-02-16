23f529cedb5ed22780b4e4cc9ee11d87b8dbe1dc
Vignesh S <svignesh0308@gmail.com>
2023-02-16 10:58:51 +0530

    Override validate of payment entry

--- parason/custompy/payment_request.py
+++ parason/custompy/payment_request.py
@@ -10,6 +10,23 @@ from erpnext.accounts.doctype.payment_entry.payment_entry import (
 
 
 class CustomPaymentRequest(PaymentRequest):
+	def validate(self):
+		if not self.is_adhoc:
+			super().validate()
+		else:
+			if self.get("__islocal"):
+				self.status = "Draft"
+			if self.reference_doctype or self.reference_name:
+				frappe.throw("Payments with references cannot be marked as ad-hoc")
+	
+	def on_submit(self):
+		if not self.is_adhoc:
+			super().on_submit()
+		else:
+			if self.payment_request_type == "Outward":
+				self.db_set("status", "Initiated")
+				return
+
 	def create_payment_entry(self, submit=True):
 		payment_entry = super().create_payment_entry(submit=submit)
 		if payment_entry.docstatus != 1 and self.payment_type:
